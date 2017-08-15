1ab860ac63fb5fcf93447280d18a46007a588cb9
Codi Lechasseur <codilechasseur@gmail.com>
2017-08-15 12:56:45 -0700

    Version bump.

--- acf-image_annotation.php
+++ acf-image_annotation.php
@@ -37,7 +37,7 @@ class acf_plugin_image_annotation {
 
 		// vars
 		$this->settings = array(
-			'version'	=> '1.0.0',
+			'version'	=> '1.0.3',
 			'url'		=> plugin_dir_url( __FILE__ ),
 			'path'		=> plugin_dir_path( __FILE__ )
 		);
